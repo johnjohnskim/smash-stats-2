8d18268203da5c8751ad49b5b74f158fcb661b3c
John Kim <johnjohnskim@gmail.com>
2014-11-30 05:03:25 -0500

    Adding highlights endpoints to api

--- routes/api.js
+++ routes/api.js
@@ -69,9 +69,9 @@ router.route('/fights')
     });
   })
 
-router.route('/fights/:fid')
+router.route('/fights/:id')
   .get(function(req, res) {
-    sql.getRow("SELECT * FROM fights WHERE id=$1", [req.params.fid], function(err, row) {
+    sql.getRow("SELECT * FROM fights WHERE id=$1", [req.params.id], function(err, row) {
       if (!row) {
         return res.end('');
       }
@@ -90,7 +90,7 @@ router.route('/fights/:fid')
     if (!field) {
       return res.end('invalid field');
     }
-    sql.query("UPDATE u_fights SET "+field+"=$1 WHERE id=$2", [value, req.params.fid], function(err) {
+    sql.query("UPDATE u_fights SET "+field+"=$1 WHERE id=$2", [value, req.params.id], function(err) {
       res.end('ok');
     });
   })
@@ -135,6 +135,94 @@ router.route('/characterfights')
       function(err, rows) {
         res.json(rows);
       });
+  })
+
+router.route('/highlights')
+  .get(function(req, res) {
+    var queries = {
+      topPlayers: "SELECT p.*, m.total, m.wins, m.winpct FROM players p \
+        INNER JOIN playermeta m ON m.id=p.id \
+        WHERE m.total>0 \
+        ORDER BY p.rating DESC, m.total DESC \
+        LIMIT 3",
+      topChars: "SELECT * FROM charactermeta \
+        WHERE total>0 \
+        ORDER BY winpct DESC, total DESC \
+        LIMIT 3",
+      bottomChars: "SELECT * FROM charactermeta \
+        WHERE total>0 \
+        ORDER BY winpct, total DESC \
+        LIMIT 3",
+      topStages: "SELECT * FROM stagemeta \
+        WHERE total>0 \
+        ORDER BY ratingchange DESC \
+        LIMIT 3"
+    };
+    sql.getMany(queries, null, function(err, results) {
+      res.json(results);
+    });
+  })
+
+router.route('/highlights/players/:id')
+  .get(function(req, res) {
+    var queries = {
+      topPlayers: "SELECT * FROM playervs v \
+            WHERE v.player1=$1 AND total>0 \
+            ORDER BY winpct DESC, total DESC \
+            LIMIT 2",
+      bottomPlayers: "SELECT * FROM playervs v \
+            WHERE v.player1=$1 AND total>0 \
+            ORDER BY winpct, total DESC \
+            LIMIT 2",
+      topChars: "SELECT * FROM characterwins w \
+            WHERE w.player=$1 AND total>0 \
+            ORDER BY winpct DESC, total DESC \
+            LIMIT 3",
+      bottomChars: "SELECT * FROM characterwins w \
+            WHERE w.player=$1 AND total>0 \
+            ORDER BY winpct, total DESC \
+            LIMIT 3",
+      topStages: "SELECT * FROM stagewins w \
+            WHERE w.player=$1 AND total>0 \
+            ORDER BY winpct DESC, total DESC \
+            LIMIT 3",
+      bottomStages: "SELECT * FROM stagewins w \
+            WHERE w.player=$1 AND total>0 \
+            ORDER BY winpct, total DESC \
+            LIMIT 3"
+    };
+    sql.getMany(queries, [req.params.id], function(err, results) {
+      res.json(results);
+    });
+  })
+
+router.route('/highlights/characters/:id')
+  .get(function(req, res) {
+    var queries = {
+      topPlayers: "SELECT * FROM characterwins w \
+        WHERE w.character=$1 AND total>0 \
+        ORDER BY winpct DESC, total DESC \
+        LIMIT 3",
+      topChars: "SELECT * FROM charactervs v \
+        WHERE v.character1=$1 AND total>0 \
+        ORDER BY winpct DESC, total DESC \
+        LIMIT 3",
+      bottomChars: "SELECT * FROM charactervs v \
+        WHERE v.character1=$1 AND total>0 \
+        ORDER BY winpct, total DESC \
+        LIMIT 3",
+      topStages: "SELECT * FROM charstagewins w \
+        WHERE w.character=$1 AND total>0 \
+        ORDER BY winpct DESC, total DESC \
+        LIMIT 3",
+      bottomStages: "SELECT * FROM charstagewins w \
+        WHERE w.character=$1 AND total>0 \
+        ORDER BY winpct, total DESC \
+        LIMIT 3"
+    };
+    sql.getMany(queries, [req.params.id], function(err, results) {
+      res.json(results);
+    });
   })
 
 module.exports = router;
