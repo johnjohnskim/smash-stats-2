02e4c197e9b2414eba25674931c65b07a4fe8ca3
John Kim <johnjohnskim@gmail.com>
2014-12-14 14:38:06 -0500

    Testing out db for free-for-alls

--- db/sql/tables.sql
+++ db/sql/tables.sql
@@ -2,6 +2,7 @@ CREATE TABLE u_characters
 (
   id serial PRIMARY KEY,
   name varchar(50) NOT NULL,
+  type varchar(10) NOT NULL,
   rating int,
   notes varchar(255)
 );
@@ -10,6 +11,7 @@ CREATE TABLE u_stages
 (
   id serial PRIMARY KEY,
   name varchar(50) NOT NULL,
+  type varchar(10) NOT NULL,
   rating int,
   notes varchar(255)
 );
@@ -18,6 +20,7 @@ CREATE TABLE u_players
 (
   id serial PRIMARY KEY,
   name varchar(50) NOT NULL,
+  type varchar(10) NOT NULL,
   rating int,
   notes varchar(255)
 );
@@ -28,12 +31,20 @@ CREATE TABLE u_fights
   date timestamp DEFAULT now(),
   player1 integer NOT NULL REFERENCES u_players,
   player2 integer NOT NULL REFERENCES u_players,
+  player3 integer NOT NULL REFERENCES u_players,
+  player4 integer NOT NULL REFERENCES u_players,
   character1 integer NOT NULL REFERENCES u_characters,
   character2 integer NOT NULL REFERENCES u_characters,
+  character3 integer NOT NULL REFERENCES u_characters,
+  character4 integer NOT NULL REFERENCES u_characters,
   stage integer NOT NULL REFERENCES u_stages,
   winner integer NOT NULL REFERENCES u_players,
+  winner2 integer NOT NULL REFERENCES u_players,
   rating1 int,
   rating2 int,
+  rating3 int,
+  rating4 int,
+  type varchar(10) NOT NULL,
   notes varchar(255)
 );
 
--- db/sql/views.sql
+++ db/sql/views.sql
@@ -14,18 +14,33 @@ CREATE VIEW fights AS
     x.player1name,
     x.player2,
     x.player2name,
+    x.player3,
+    x.player3name,
+    x.player4,
+    x.player4name,
     x.character1,
     x.character1name,
     x.character2,
     x.character2name,
+    x.character3,
+    x.character3name,
+    x.character4,
+    x.character4name,
     x.stage,
     x.stagename,
     x.winner,
     p.name AS winnername,
+    x.winner2,
+    q.name AS winner2name,
     x.winnerchar,
     c.name AS winnercharname,
+    x.winner2char,
+    d.name AS winner2charname,
     x.rating1,
     x.rating2,
+    x.rating3,
+    x.rating4,
+    x.type,
     x.notes
   FROM ( SELECT DISTINCT
             f.id,
@@ -34,29 +49,56 @@ CREATE VIEW fights AS
             p1.name AS player1name,
             f.player2,
             p2.name AS player2name,
+            f.player3,
+            p3.name AS player3name,
+            f.player4,
+            p4.name AS player4name,
             f.character1,
             c1.name AS character1name,
             f.character2,
             c2.name AS character2name,
+            f.character3,
+            c3.name AS character3name,
+            f.character4,
+            c4.name AS character4name,
             f.stage,
             s.name AS stagename,
             f.winner,
+            f.winner2,
             f.rating1,
             f.rating2,
+            f.rating3,
+            f.rating4,
+            f.type,
             f.notes,
             CASE
               WHEN f.winner = f.player1 THEN f.character1
               WHEN f.winner = f.player2 THEN f.character2
+              WHEN f.winner = f.player3 THEN f.character3
+              WHEN f.winner = f.player4 THEN f.character4
               ELSE NULL
-            END AS winnerchar
+            END AS winnerchar,
+            CASE
+              WHEN f.winner2 = f.player1 THEN f.character1
+              WHEN f.winner2 = f.player2 THEN f.character2
+              WHEN f.winner2 = f.player3 THEN f.character3
+              WHEN f.winner2 = f.player4 THEN f.character4
+              ELSE NULL
+            END AS winner2char
           FROM u_fights f
             LEFT JOIN players p1 ON p1.id = f.player1
             LEFT JOIN players p2 ON p2.id = f.player2
+            LEFT JOIN players p3 ON p3.id = f.player3
+            LEFT JOIN players p4 ON p4.id = f.player4
             LEFT JOIN characters c1 ON c1.id = f.character1
             LEFT JOIN characters c2 ON c2.id = f.character2
+            LEFT JOIN characters c3 ON c3.id = f.character3
+            LEFT JOIN characters c4 ON c4.id = f.character4
             INNER JOIN stages s ON s.id = f.stage) x
     INNER JOIN players p ON p.id = x.winner
-    INNER JOIN characters c ON c.id = x.winnerchar;
+    LEFT JOIN players q ON q.id = x.winner2
+    INNER JOIN characters c ON c.id = x.winnerchar
+    LEFT JOIN characters d ON d.id = x.winner2char;
 
 CREATE VIEW events AS
 SELECT * FROM u_events;
