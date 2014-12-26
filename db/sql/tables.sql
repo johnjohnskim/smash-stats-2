dc41dd5965f1e144439b5a54c638634a1ae66531
John Kim <johnjohnskim@gmail.com>
2014-12-25 22:53:02 -0500

    Table updates

--- db/sql/tables.sql
+++ db/sql/tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE u_characters
 (
   id serial PRIMARY KEY,
   name varchar(50) NOT NULL,
-  type varchar(10) NOT NULL,
+  type varchar(10),
   rating int,
   notes varchar(255)
 );
@@ -11,7 +11,7 @@ CREATE TABLE u_stages
 (
   id serial PRIMARY KEY,
   name varchar(50) NOT NULL,
-  type varchar(10) NOT NULL,
+  type varchar(10),
   rating int,
   notes varchar(255)
 );
@@ -20,7 +20,7 @@ CREATE TABLE u_players
 (
   id serial PRIMARY KEY,
   name varchar(50) NOT NULL,
-  type varchar(10) NOT NULL,
+  type varchar(10),
   rating int,
   notes varchar(255)
 );
@@ -31,15 +31,15 @@ CREATE TABLE u_fights
   date timestamp DEFAULT now(),
   player1 integer NOT NULL REFERENCES u_players,
   player2 integer NOT NULL REFERENCES u_players,
-  player3 integer NOT NULL REFERENCES u_players,
-  player4 integer NOT NULL REFERENCES u_players,
+  player3 integer REFERENCES u_players,
+  player4 integer REFERENCES u_players,
   character1 integer NOT NULL REFERENCES u_characters,
   character2 integer NOT NULL REFERENCES u_characters,
-  character3 integer NOT NULL REFERENCES u_characters,
-  character4 integer NOT NULL REFERENCES u_characters,
+  character3 integer REFERENCES u_characters,
+  character4 integer REFERENCES u_characters,
   stage integer NOT NULL REFERENCES u_stages,
   winner integer NOT NULL REFERENCES u_players,
-  winner2 integer NOT NULL REFERENCES u_players,
+  winner2 integer REFERENCES u_players,
   rating1 int,
   rating2 int,
   rating3 int,
