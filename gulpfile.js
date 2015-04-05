af34f19476a0c37968496ca33792083c90d6373b
John Kim <johnjohnskim@gmail.com>
2015-04-05 11:19:27 -0400

    Adding sourcemap test code

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var isProduction = process.env.NODE_ENV == 'production';
 var gulp = require('gulp');
 var path = require('path');
 var concat = require('gulp-concat');
@@ -8,48 +9,42 @@ var browserify = require('gulp-browserify');
 var reactify = require('reactify');
 var react = require('gulp-react');
 var uglify = require('gulp-uglify');
-var livereload = process.env.NODE_ENV != 'production' ? require('gulp-livereload') : null;
-var nodemon = process.env.NODE_ENV != 'production' ? require('gulp-nodemon') : null;
+// var sourcemaps = require('gulp-sourcemaps');
+var livereload = isProduction ? null : require('gulp-livereload');
+var nodemon = isProduction ? null : require('gulp-nodemon');
 
 // compiled files are created in build/
 // comment out the minifyCss() or uglify() lines to debug (sourcemaps aren't currently working)
 gulp.task('less', function() {
   gulp.src(['./bower_components/skeleton/css/normalize.css', './bower_components/skeleton/css/skeleton.css', './less/app.less'])
+    // .pipe(sourcemaps.init())
     .pipe(less())
     .pipe(concat('app.min.css'))
     .pipe(minifyCss())
+    // .pipe(sourcemaps.write('../maps'))
     .pipe(gulp.dest('./build/css'))
 });
 
 gulp.task('react', function() {
   gulp.src('react/*.jsx')
+    // .pipe(sourcemaps.init())
     .pipe(react())
     .pipe(uglify())
     .pipe(rename(function(path) {
       path.extname = '.min.js';
     }))
+    // .pipe(sourcemaps.write('../maps'))
     .pipe(gulp.dest('./build/js'))
 });
 
-// gulp.task('reactify', function() {
-//   gulp.src('react/*.jsx', {read: false})
-//     .pipe(browserify({
-//       transform: ['reactify'],
-//       extensions: ['.jsx'],
-//     }))
-//     .pipe(uglify())
-//     .pipe(rename(function(path) {
-//       path.extname = '.min.js';
-//     }))
-//     .pipe(gulp.dest('./build/js'))
-// });
-
 gulp.task('uglify', function() {
   gulp.src('public/js/*.js')
+    // .pipe(sourcemaps.init())
     .pipe(uglify())
     .pipe(rename(function(path) {
       path.extname = '.min.js';
     }))
+    // .pipe(sourcemaps.write('../maps'))
     .pipe(gulp.dest('./build/js'))
 });
 
@@ -69,7 +64,7 @@ gulp.task('watch', function() {
   gulp.watch(['./less/*.less'], ['less']);
   gulp.watch(['./react/*.jsx'], ['react']);
   // gulp.watch(['./react/*.jsx'], ['react']);
-  gulp.watch(['./build/**'])
+  gulp.watch(['./build/**', '!./build/maps/**'])
     .on('change', livereload.changed)
 });
 
